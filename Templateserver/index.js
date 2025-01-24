a179d9b6c516e3ff28a7381acd2f5ec872ce56b9
subhamdey1234 <sdey06384@gmail.com>
2025-01-24 16:22:10 +0530

    first commit

--- Templateserver/index.js
+++ Templateserver/index.js
@@ -5,17 +5,21 @@ app.use(express.static("E:/m17jspider/express.js/Templateserver/public"));
 app.set("view engine","ejs");
 app.set("views","./public");
 
-//middleware
-app.use(express.json());
+//--------------Middleware------------
+app.use(express.json());     //Json Parser
+app.use(express.static("./public")) //Serving Static Files
 
 
-
+// ----------API'S--------------
 app.get("/",(req,res)=>{
     res.render("index.ejs",{name:"Subham Dey",age:"23",address:"Bangalore Marathalli"});
 })
 
 app.get("/about", (req, res) => {
-    res.render("index.ejs", { name: "Rahul", age: 20, address: "Delhi" })
+    res.render("about.ejs", { fruits:["Apple","Banana","Mango","Grapes"]  })
+})
+app.get("/login",(req,res)=>{
+    res.render("login.ejs",{ title:"Log+in Page" })
 })
 
 
